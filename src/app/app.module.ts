a1fae51c9932526fb8de4b9022264745175b9fa3
tomivr55 <tomas.z.minov@gmail.com>
2024-03-27 20:00:13 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MainComponent } from './main/main.component';
 import { CarThemeListComponent } from './car-theme-list/car-theme-list.component';
 import { CarPostsListComponent } from './car-posts-list/car-posts-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,12 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: 'short' },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/car-posts-list/car-posts-list.component.ts
+++ src/app/car-posts-list/car-posts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { CarPost } from '../types/carPost';
 
 @Component({
   selector: 'app-car-posts-list',
@@ -8,9 +9,11 @@ import { ApiService } from '../api.service';
 })
 export class CarPostsListComponent implements OnInit {
   constructor(private api: ApiService) {}
+  themeComent: CarPost[] = [];
   ngOnInit(): void {
-    this.api.getCarPosts(4).subscribe((post) => {
+    this.api.getCarPosts().subscribe((post) => {
       console.log(post);
+      this.themeComent = post;
     });
   }
 }
